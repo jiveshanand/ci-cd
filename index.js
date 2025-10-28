fc13831110792992d1c034c5d80da72ea51c8216
Jivesh Anand <jivesh.11@gmail.com>
2025-10-27 19:02:24 -0700

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("🎉 CI/CD Demo: Hello from GitHub Actions!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
